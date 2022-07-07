8bb4a94748786de5eca907cd85608b1c243e509f
Busra Kizilaslan Yalcin <busra_kizilaslan@hotmail.com>
2022-07-07 23:59:12 +0300

    Initial commit

---
+++ app/src/main/java/com/busrayalcin/mvvmkullanimi/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.busrayalcin.mvvmkullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.busrayalcin.mvvmkullanimi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tasarim:ActivityMainBinding
+    private val viewModel:MainActivityViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tasarim = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        tasarim.mainActivityNesnesi = this
+
+        viewModel.sonuc.observe(this){
+            tasarim.hesaplamaSonucu = it
+        }
+    }
+
+    fun buttonToplamaTikla(alinanSayi1:String,alinanSayi2:String){
+        viewModel.toplamaYap(alinanSayi1,alinanSayi2)
+    }
+
+    fun buttonCarpmaTikla(alinanSayi1:String,alinanSayi2:String){
+        viewModel.carpmaYap(alinanSayi1,alinanSayi2)
+    }
+}
